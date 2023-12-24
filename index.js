14085942d338dd5b92f2b7d3108e4a506d7dccb9
program1519 <109532636+program1519@users.noreply.github.com>
2023-12-24 20:40:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,74 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const fs = require('fs');
+    const express = require('express');
+    const app = express();
+    const http = require('http');
+    const server = http.createServer(app);
+    const { Server } = require("socket.io");
+    const io = new Server(server);
+    const fs = require('fs');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => { 
-  console.log('a user connected');
-
-  // อ่านประวัติแชทจากไฟล์ล็อก (ถ้ามี)
-  fs.readFile('chatlog.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      // ส่งประวัติแชทให้ผู้ใช้ที่เพิ่งเชื่อมต่อ
-      socket.emit('chat history', data.split('\n'));
-    }
-  });
-
-  socket.on('disconnect', () => { 
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', (msg) => { 
-    console.log('message: ' + msg);
-    
-    // เพิ่มข้อความในประวัติแชทและบันทึกลงในไฟล์ล็อก
-    const logMessage = new Date().toLocaleString() + ': ' + msg;
-    fs.appendFile('chatlog.txt', logMessage + '\n', (err) => {
-      if (err) {
-        console.error(err);
-      }
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
     });
 
-    // ส่งข้อความที่ผู้ใช้ส่งมาให้กับผู้ใช้ที่เชื่อมต่อทุกคน
-    io.emit('chat message', logMessage);
+    io.on('connection', (socket) => { 
+      console.log('a user connected');
+
+      // อ่านประวัติแชทจากไฟล์ (ถ้ามี)
+      fs.readFile('chatlog.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // ส่งประวัติแชทให้ผู้ใช้ที่เพิ่งเชื่อมต่อ
+          socket.emit('chat history', data.split('\n'));
+        }
+      });
+
+      socket.on('disconnect', () => { 
+        console.log('user disconnected');
+      });
+
+      socket.on('chat message', (msg) => { 
+        console.log('message: ' + msg);
+
+ // ตรวจสอบว่าข้อความไม่เป็นสแปม (ตัวอย่าง: ไม่เกิน 5 ตัวอักษร)
+    if (msg && msg.length <= 5) {
+      
+      const logMessage = 'Guest: ' + msg; 
+      fs.appendFile('chatlog.txt', logMessage + '\n', (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+
+      
+      io.emit('chat message', logMessage);
+    } else {
+      // ถ้าข้อความเป็นสแปมหรือยาวเกินไป ให้บันทึกลงในไฟล์ logreport.txt
+      const spamMessage = 'มีคน spam หรือ long message: ' + msg;
+      fs.appendFile('logreport.txt', spamMessage + '\n', (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
   });
 });
 
-server.listen(80, () => {
-  console.log('server run...');
+    server.listen(80, () => {
+      console.log('server run...');
+    });
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    if (msg === '/report') {
+      const logMessage = 'มีผู้ใช้report';
+      fs.appendFile('logreport.txt', logMessage + '\n', (err) => {
+        if (err) throw err;
+        console.log('Report logged.');
+      });
+    } else {
+      io.emit('chat message', msg);
+    }
+  });
 });
+
